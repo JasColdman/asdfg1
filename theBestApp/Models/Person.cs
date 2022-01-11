c69522aa47221079d70a475bb7cfbed45de53e87
Виктор Терентьев <jaslolman@gmail.com>
2022-01-12 02:45:40 +0500

    test

--- theBestApp/Models/Person.cs
+++ theBestApp/Models/Person.cs
@@ -5,7 +5,7 @@
     public class Person
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Name1 { get; set; }
         public int Age { get; set; }
     }
 }
