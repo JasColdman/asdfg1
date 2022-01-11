8d7f414f56ff9280b80d667763259043405d9419
Виктор Терентьев <jaslolman@gmail.com>
2022-01-12 02:32:41 +0500

    Добавьте файлы проекта.

---
+++ theBestApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using theBestApp.Models;
+
+namespace theBestApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<theBestApp.Models.Person> Person { get; set; }
+    }
+}
---
+++ theBestApp/Models/Person.cs
@@ -0,0 +1,11 @@
+﻿namespace theBestApp.Models
+    
+    //Человек
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
